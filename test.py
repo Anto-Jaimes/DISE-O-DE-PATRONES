7b0d8a20df64948a4ccb9b7990c254661fd2a431
Antonela <antonelajaimesdaza@gmail.com>
2026-07-15 18:45:51 -0500

    Clean up root repository of temporary scripts and files, downgrade Spring Boot to 3.3.0

--- test.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-req = urllib.request.Request('http://localhost:8080/')
-req.add_header('Cookie', 'JSESSIONID=dummy')
-try:
-    res = urllib.request.urlopen(req)
-    print(res.read().decode('utf-8'))
-except Exception as e:
-    print(e)
